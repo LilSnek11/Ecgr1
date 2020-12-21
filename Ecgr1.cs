bb01bf81d1b7d3b4a11aafc60bc31700782fbcb9
Szello Istvan <68303099+LilSnek11@users.noreply.github.com>
2020-12-21 03:01:06 -0800

    Add files via upload

---
+++ Ecgr1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Program
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a,b;
+            
+            double c,n,m;
+            a = Console.ReadLine();
+            n = Convert.ToInt32(a);
+            b = Console.ReadLine();
+            m = Convert.ToInt32(b);
+            c = -n/ m;
+            Console.Write("x= {0}",c);
+
+
+
+
+
+
+            
+
+        }
+    }
+}
